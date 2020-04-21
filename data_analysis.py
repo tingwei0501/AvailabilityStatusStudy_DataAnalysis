792f87c289d33f321c78cf5c18a6eaefe6f768a1
tingwei0501 <tingwei0501.cs02@g2.nctu.edu.tw>
2020-04-21 16:43:54 +0800

    Add files via upload

--- data_analysis.py
+++ data_analysis.py
@@ -41,13 +41,157 @@ with open('analysis data(python).csv', newline='') as csvfile:
     # tma = systemStatus.text_middle_available()
     manager = Manager(rows)
 
-    text_keep_Highly_Available = 0
-    for data in manager.dataTable:
-        
-        # if data.getSystemStatus()=='text_HighlyAvailable' and data.getSelfReportStatus()=='text_HighlyAvailable':
-        #     text_keep_Highly_Available += 1
+    system_text_highly_available = []
+    system_text_middle_available = []
+    system_text_middle_unavailable = []
+    system_text_highly_unavailable = []
+
+    system_digit_highly_available = []
+    system_digit_middle_available = []
+    system_digit_middle_unavailable = []
+    system_digit_highly_unavailable = []
+
+    system_graphic_highly_available = []
+    system_graphic_middle_available = []
+    system_graphic_middle_unavailable = []
+    system_graphic_highly_unavailable = []
     
-    print ("text_keep_Highly_Available: ", text_keep_Highly_Available)
+    user_text_highly_available = []
+    user_text_middle_available = []
+    user_text_middle_unavailable = []
+    user_text_highly_unavailable = []
+
+    user_digit_highly_available = []
+    user_digit_middle_available = []
+    user_digit_middle_unavailable = []
+    user_digit_highly_unavailable = []
+
+    user_graphic_highly_available = []
+    user_graphic_middle_available = []
+    user_graphic_middle_unavailable = []
+    user_graphic_highly_unavailable = []
+
+    text_keep_highly_available = 0
+    text_highly_available_2_middle_available = 0
+    text_highly_available_2_unavailable = 0
+    text_highly_available_2_digit_highly_available = 0
+    text_highly_available_2_digit_middle_available = 0
+    text_highly_available_2_digit_middle_unavailable = 0
+    text_highly_available_2_digit_highly_unavailable = 0
+    text_highly_available_2_graphic_highly_available = 0
+    text_highly_available_2_graphic_middle_available = 0
+    text_highly_available_2_graphic_middle_unavailable = 0
+    text_highly_available_2_graphic_highly_unavailable = 0
+
+    
+    for data in manager.dataTable:
+        if data.getSystemStatus()=='text_HighlyAvailable':
+            if data.getSelfReportStatus()=='text_HighlyAvailable':
+                # 存情境資料??
+                text_keep_highly_available += 1
+            elif data.getSelfReportStatus()=='text_MiddleAvailable':
+                text_highly_available_2_middle_available += 1
+            elif data.getSelfReportStatus()=='text_MiddleUnavailable' or data.getSelfReportStatus()=='text_HighlyUnavailable':
+                text_highly_available_2_unavailable += 1
+            # 改用數字
+            elif data.getSelfReportStatus()=='digit_HighlyAvailable':
+                text_highly_available_2_digit_highly_available += 1
+            elif data.getSelfReportStatus()=='digit_MiddleAvailable':
+                text_highly_available_2_digit_middle_available += 1
+            elif data.getSelfReportStatus()=='digit_MiddleUnavailable':
+                text_highly_available_2_digit_middle_unavailable += 1
+            elif data.getSelfReportStatus()=='digit_HighlyUnavailable':
+                text_highly_available_2_digit_highly_unavailable += 1
+            # 改用圖像
+            elif data.getSelfReportStatus()=='graphic_HighlyAvailable':
+                text_highly_available_2_graphic_highly_available += 1
+            elif data.getSelfReportStatus()=='graphic_MiddleAvailable':
+                text_highly_available_2_graphic_middle_available += 1
+            elif data.getSelfReportStatus()=='graphic_MiddleUnavailable':
+                text_highly_available_2_graphic_middle_unavailable += 1
+            elif data.getSelfReportStatus()=='graphic_HighlyUnavailable':
+                text_highly_available_2_graphic_highly_unavailable += 1
+        
+            # if data.getSystemStatus()=='text_HighlyAvailable':
+            #     system_text_highly_available.append(data)
+            # elif data.getSystemStatus()=='text_MiddleAvailable':
+            #     system_text_middle_available.append(data)
+            # elif data.getSystemStatus()=='text_MiddleUnavailable':
+            #     system_text_middle_unavailable.append(data)
+            # elif data.getSystemStatus()=='text_HighlyUnavailable':
+            #     system_text_highly_unavailable.append(data)
+            # # digit
+            # elif data.getSystemStatus()=='digit_HighlyAvailable':
+            #     system_digit_highly_available.append(data)
+            # elif data.getSystemStatus()=='digit_MiddleAvailable':
+            #     system_digit_middle_available.append(data)
+            # elif data.getSystemStatus()=='digit_MiddleUnavailable':
+            #     system_digit_middle_unavailable.append(data)
+            # elif data.getSystemStatus()=='digit_HighlyUnavailable':
+            #     system_digit_highly_unavailable.append(data)
+            # # graphic
+            # elif data.getSystemStatus()=='graphic_HighlyAvailable':
+            #     system_graphic_highly_available.append(data)
+            # elif data.getSystemStatus()=='graphic_MiddleAvailable':
+            #     system_graphic_middle_available.append(data)
+            # elif data.getSystemStatus()=='graphic_MiddleUnavailable':
+            #     system_graphic_middle_unavailable.append(data)
+            # elif data.getSystemStatus()=='graphic_HighlyUnavailable':
+            #     system_graphic_highly_unavailable.append(data)
+            # else:
+            #     print ("error: SystemStatus")
+
+            # if data.getSelfReportStatus()=='text_HighlyAvailable':
+            #     user_text_highly_available.append(data)
+            # elif data.getSelfReportStatus()=='text_MiddleAvailable':
+            #     user_text_middle_available.append(data)
+            # elif data.getSelfReportStatus()=='text_MiddleUnavailable':
+            #     user_text_middle_unavailable.append(data)
+            # elif data.getSelfReportStatus()=='text_HighlyUnavailable':
+            #     user_text_highly_unavailable.append(data)
+            # # digit
+            # elif data.getSelfReportStatus()=='digit_HighlyAvailable':
+            #     user_digit_highly_available.append(data)
+            # elif data.getSelfReportStatus()=='digit_MiddleAvailable':
+            #     user_digit_middle_available.append(data)
+            # elif data.getSelfReportStatus()=='digit_MiddleUnavailable':
+            #     user_digit_middle_unavailable.append(data)
+            # elif data.getSelfReportStatus()=='digit_HighlyUnavailable':
+            #     user_digit_highly_unavailable.append(data)
+            # # graphic
+            # elif data.getSelfReportStatus()=='graphic_HighlyAvailable':
+            #     user_graphic_highly_available.append(data)
+            # elif data.getSelfReportStatus()=='graphic_MiddleAvailable':
+            #     user_graphic_middle_available.append(data)
+            # elif data.getSelfReportStatus()=='graphic_MiddleUnavailable':
+            #     user_graphic_middle_unavailable.append(data)
+            # elif data.getSelfReportStatus()=='graphic_HighlyUnavailable':
+            #     user_graphic_highly_unavailable.append(data)
+            # else:
+            #     print ("error: SelfReportStatus") 
+
+        
+    # s = Manager(system_text_highly_available)
+    # u = Manager(user_text_highly_available)
+    # text_keep_highly_available_list = s & u
+    # print (text_keep_highly_available_list)
+    # for i in text_keep_highly_available_list:
+    #     print (i)
+
+    print ("text_keep_highly_available: ", text_keep_highly_available)
+    print ("text_highly_available_2_middle_available: ", text_highly_available_2_middle_available)
+    print ("text_highly_available_2_unavailable: ", text_highly_available_2_unavailable)
+
+    print ("text_highly_available_2_digit_highly_available: ", text_highly_available_2_digit_highly_available)
+    print ("text_highly_available_2_digit_middle_available: ", text_highly_available_2_digit_middle_available)
+    print ("text_highly_available_2_digit_middle_unavailable: ", text_highly_available_2_digit_middle_unavailable)
+    print ("text_highly_available_2_digit_highly_unavailable: ", text_highly_available_2_digit_highly_unavailable)
+
+    print ("text_highly_available_2_graphic_highly_available: ", text_highly_available_2_graphic_highly_available)
+    print ("text_highly_available_2_graphic_middle_available: ", text_highly_available_2_graphic_middle_available)
+    print ("text_highly_available_2_graphic_middle_unavailable: ", text_highly_available_2_graphic_middle_unavailable)
+    print ("text_highly_available_2_graphic_highly_unavailable: ", text_highly_available_2_graphic_highly_unavailable)
+
 
 
 
